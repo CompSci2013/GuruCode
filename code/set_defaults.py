c5e310bf1df265c6b4db85368f06488ee227e5c1
ec2 <ec2@example.com>
2021-12-06 21:08:29 +0000

    restore function body

--- code/set_defaults.py
+++ code/set_defaults.py
@@ -1,2 +1,5 @@
-def setdefault_example():
-    pass
+etdefault_example():
+    std_dict = dict()
+    for k,v in enumerate(range(5)):
+        std_dict.setdefault(k,[]).append(v)
+    return std_dict
